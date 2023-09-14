d939b78020cbcedf90c22532e80c759202c51da7
ThomasBacheley <thom.bacheley@gmail.com>
2023-09-14 13:40:12 +0200

    update

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -37,7 +37,7 @@ router.get("/deleteParty", (req, res) => {
     .fetchMessage(embed_id)
     .then((msg) => msg.delete());*/
 
-    res.json({ message: "Ok", status: 200 });
+  res.json({ message: "Ok", status: 200 });
 });
 
 module.exports = router;
